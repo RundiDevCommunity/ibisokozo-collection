f9089dd135103a9d92eb8656c2b3a7582f064e84
dondelice257 <dondelicedushime@gmail.com>
2023-10-01 00:05:19 +0200

    initial commit

---
+++ src/app/services/ibisokozo.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/compat/firestore'
+import {AngularFireStorage} from '@angular/fire/compat/storage'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IbisokozoService {
+
+  constructor(
+    private afs : AngularFirestore,
+    private store : AngularFireStorage) { }
+
+
+
+  addIgisokozo(igisokozo:any){
+    const igisokozoId = this.afs.createId();
+    igisokozo.id=igisokozoId;
+    return this.afs.collection('ibisokozo').add(igisokozo);
+  }
+
+  getIbisokozoList(){
+    return this.afs.collection('ibisokozo').snapshotChanges()
+  }
+}
